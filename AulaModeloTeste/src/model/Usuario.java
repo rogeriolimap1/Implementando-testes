a97dd17d668763dfa1a7f848f9eca7f7349f263b
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-04-21 18:17:57 -0300

    primeira versao

---
+++ AulaModeloTeste/src/model/Usuario.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Usuario {
+	private String nome;
+	private String email;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
