37c0e83d9ffa14e7c29aebe00d9ca67472538454
LG <LG@LG-PC>
2022-09-08 20:03:43 -0300

    Commit 09-08

--- projetoAvalidador-ModeloTeste/src/test/java/org/iftm/ProjetoAvalidadorModeloTesteApplicationTests.java
+++ projetoAvalidador-ModeloTeste/src/test/java/org/iftm/ProjetoAvalidadorModeloTesteApplicationTests.java
@@ -1,13 +1,69 @@
-package org.iftm;
+package org.iftm.entity;
 
+import org.iftm.entities.Avaliador;
+import org.iftm.entities.Leilao;
+import org.iftm.entities.Usuario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProjetoAvalidadorModeloTesteApplicationTests {
+public class AvaliadorTest {
+	private Usuario usuario1;
+	private Usuario usuario2;
+	private Usuario usuario3;
+	private Usuario usuario4;
+	private Leilao leilao;
+	private Avaliador avaliador;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	public void instanciarObjetos() {
+		usuario1 = new Usuario("Shirley");
+		usuario2 = new Usuario("Edna");
+		usuario3 = new Usuario("Laiane");
+		usuario4 = new Usuario("Cristina");
+		leilao = new Leilao("Computador Apple");
+		avaliador = new Avaliador();
 	}
-
+	
+	@Test
+	public void testaBuscarMaiorLanceSemOrdem() {
+		//Entradas > criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance2 = new Lance(usuario2, 400.0000001);
+		Lance lance3 = new Lance(usuario3, 250);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		leilao.propoe(lance3);
+		
+		double resultadoEsperado = 400.00;
+		
+		//execução
+		avaliador.avalia(leilao); 
+		double resultadoObtido = avaliador.getMaiorLance();
+		
+		//comparação
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+	}
+	
+	@Test
+	public void testaBuscarMaiorLanceOrdemCrescente() {
+		//Entradas > criar os lances e o resultado esperado
+		Lance lance1 = new Lance(usuario1, 300);
+		Lance lance3 = new Lance(usuario3, 250);
+		Lance lance2 = new Lance(usuario2, 400);
+		leilao.propoe(lance3);
+		leilao.propoe(lance1);
+		leilao.propoe(lance2);
+		
+		
+		double resultadoEsperado = 400.00;
+		
+		//execução
+		avaliador.avalia(leilao); 
+		double resultadoObtido = avaliador.getMaiorLance();
+		
+		//comparação
+		Assertions.assertEquals(resultadoEsperado, resultadoObtido, 0.0001);
+				
+		
+	}
 }
